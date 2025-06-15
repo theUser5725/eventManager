5f6e6a8afcafe4b9ee5fdd00521b673d825b229e
theUser5725 <varga.alan5725@gmail.com>
2025-06-15 16:40:10 -0300

    creacion de la entidad Reunion

---
+++ eventManager/WinFormsApp1/Entidades/Conexion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using FormularioDinamico.Modelos;
+
+namespace FormularioDinamico.Controladores
+{
+    class Conexion
+    {
+        public static string cadena = "Data Source=estacionamiento.sqlite";
+
+        private static SQLiteConnection connection = new SQLiteConnection(cadena);
+
+        public static void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public static void CloseConnection()
+        {
+            connection.Close();
+        }
+
+        //constructor de la variable connection  
+        public static SQLiteConnection Connection
+        {
+            set
+            {
+                connection = value;
+            }
+            get
+            {
+                return connection;
+            }
+        }
+    }
+}
