e3770bef5505d88d01cdac3d1bbcdbd04ce05ae8
Cristian <cristian.osorio@uap.edu.>
2025-06-17 15:04:11 -0300

    smallFix

--- eventManager/WinFormsApp1/Program.cs
+++ eventManager/WinFormsApp1/Program.cs
@@ -1,17 +1,19 @@
+using WinFormsApp1.Controladores;
+
 namespace WinFormsApp1
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+ 
         [STAThread]
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            Conexion.OpenConnection();
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            Conexion.CloseConnection();
         }
     }
 }
