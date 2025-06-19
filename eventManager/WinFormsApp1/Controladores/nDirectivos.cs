98ce7638713aa8bffd2ea75c838f2fd4223ecd3c
belenbuzatto <maria.buzatto@uap.edu.ar>
2025-06-19 10:07:55 -0300

    Signed-off-by: belenbuzatto <maria.buzatto@uap.edu.ar>

---
+++ eventManager/WinFormsApp1/Controladores/nDirectivos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using tp4_Prueba.Modelos;
+
+namespace tp4_Prueba.Controladores
+{
+    internal class nDirectivos
+    {
+        public static Directivos getByPk(int idReunion, int idParticipante)
+        {
+            Directivos directivo = null;
+
+            SQLiteCommand cmd = new SQLiteCommand("SELECT idReunion, idParticipante, idCatDirectiva FROM Directivos WHERE idReunion = @idReunion AND idParticipante = @idParticipante");
+            cmd.Parameters.Add(new SQLiteParameter("@idReunion", idReunion));
+            cmd.Parameters.Add(new SQLiteParameter("@idParticipante", idParticipante));
+            cmd.Connection = Conexion.Connection;
+
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                directivo = new Directivos(
+                    reader.GetInt32(0),
+                    reader.GetInt32(1),
+                    reader.GetInt32(2)
+                );
+            }
+
+            return directivo;
+        }
+
+        public static List<Directivos> getAllByReunionId(int idReunion)
+        {
+            List<Directivos> directivos = new List<Directivos>();
+
+            SQLiteCommand cmd = new SQLiteCommand("SELECT idReunion, idParticipante, idCatDirectiva FROM Directivos WHERE idReunion = @idReunion");
+            cmd.Parameters.Add(new SQLiteParameter("@idReunion", idReunion));
+            cmd.Connection = Conexion.Connection;
+
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Directivos directivo = new Directivos(
+                    reader.GetInt32(0),
+                    reader.GetInt32(1),
+                    reader.GetInt32(2)
+                );
+
+                directivos.Add(directivo);
+            }
+
+            return directivos;
+        }
+    }
+}
---
+++ eventManager/WinFormsApp1/Modelos/Directivos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+idReunion
+idParticipante
+idCatDirectiva
+ */
+namespace tp4_Prueba.Modelos
+{
+    internal class Directivos
+    {
+        public int IdReunion { get; set; }
+        public int IdParticipante { get; set; }
+        public int IdCatDirectiva { get; set; }
+
+        public Directivos(int idReunion, int idParticipante, int idCatDirectiva) 
+        {
+            IdReunion = idReunion;
+            IdParticipante = idParticipante;
+            IdCatDirectiva = idCatDirectiva;
+        }
+
+    }
+}
