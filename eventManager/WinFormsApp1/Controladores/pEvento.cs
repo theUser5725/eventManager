b615b589798fbc12eff6a132a5f8553d2485fe9f
Cristian <cristian.osorio@uap.edu.>
2025-06-19 22:58:49 -0300

    Adicion de la db a la carpeta debug, asi como su la exclusion de su path al gitIgnore, junto a small fixes

--- eventManager/WinFormsApp1/Controladores/pEvento.cs
+++ eventManager/WinFormsApp1/Controladores/pEvento.cs
@@ -63,14 +63,15 @@ namespace WinFormsApp1.Controladores
             }
 
             // Cargar participantes
-            evento.Participantes = pParticipante.GetAllByEventoId(idEvento)
+            evento.Participantes = pParticipante.GetAllByEventoId(idEvento);
 
             // Cargar reuniones
             evento.Reuniones = pReunion.GetAllByEventoId(idEvento);
-        }
-
             Conexion.CloseConnection();
             return evento;
         }
+
+            
+        
     }
 }
